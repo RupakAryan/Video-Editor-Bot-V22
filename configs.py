6d9a84124a8eab45c1754186e9433b528abef3fa
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-09-01 20:16:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 import os
 
 API_ID = int(os.getenv("API_ID", 29978901))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
+API_HASH = os.getenv("API_HASH", "500fc876c5356cf04ed3698912dc2edf")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8484698189:AAERygnLHP4vfFqaXlA4G8FHTcVMsMLHfzo")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://rupakaryanaryan:rupakaryanaryan@cluster0.05xkjx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ))
